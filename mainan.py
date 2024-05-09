8242290e4e88efe94418facd13941dd8d4454eb0
Meph1sto14 <marvelsanjaya7@gmail.com>
2024-05-09 14:35:16 +0700

    Input nama dan buat Terima kasih

--- mainan.py
+++ mainan.py
@@ -1,9 +1,10 @@
-def tokomainan():
-        mainan = {
-            "1": "hotwheels",
-            "2": "lego batman",
-            "3": "boneka barbie",
-            "4": "nerf pistol",
+def main():
+    # Kamus mainan dan harga
+    mainan = {
+        "1": "hotwheels",
+        "2": "lego batman",
+        "3": "boneka barbie",
+        "4": "nerf pistol",
             "5": "nerf shotgun",
             "6": "nerf rifle",
             "7": "nerf sniper",
@@ -19,11 +20,11 @@ def tokomainan():
             "17": "monopoli",
             "18": "yoyo",
             "19": "rubik",
-        }
-        harga = {
-                "1": 55000,
-                "2": 750000,
-                "3": 700000,
+    }
+    harga = {
+        "1": 55000,
+        "2": 750000,
+        "3": 700000,
                 "4": 310000,
                 "5": 500000,
                 "6": 600000,
@@ -40,4 +41,15 @@ def tokomainan():
                 "17": 25000,
                 "18": 15000,
                 "19": 45000,
-        }
+    }
+
+    # Menyambut pengguna
+#     name = input("Masukkan nama Anda: ")
+#     print(f"Halo {name}, selamat datang di Toko Mainan!")
+
+
+    # Pesan perpisahan
+    print("\nTerima kasih telah berbelanja di Toko Mainan!")
+
+if __name__ == "__main__":
+    main()
